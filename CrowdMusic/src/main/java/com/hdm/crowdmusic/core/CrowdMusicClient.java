9e78c7ba6c9cd0017c256483ae1e89a07e190950
Hannes Pernpeintner <hannes.pernpeintner@gmx.de>
2013-11-05 17:53:19 +0100

    Implemented basic client service and corresponding activities

--- CrowdMusic/src/main/java/com/hdm/crowdmusic/core/CrowdMusicClient.java
+++ CrowdMusic/src/main/java/com/hdm/crowdmusic/core/CrowdMusicClient.java
@@ -4,26 +4,21 @@ import org.teleal.cling.binding.LocalServiceBindingException;
 import org.teleal.cling.binding.annotations.AnnotationLocalServiceBinder;
 import org.teleal.cling.model.DefaultServiceManager;
 import org.teleal.cling.model.ValidationException;
-import org.teleal.cling.model.meta.DeviceDetails;
-import org.teleal.cling.model.meta.DeviceIdentity;
-import org.teleal.cling.model.meta.LocalDevice;
-import org.teleal.cling.model.meta.LocalService;
-import org.teleal.cling.model.meta.ManufacturerDetails;
-import org.teleal.cling.model.meta.ModelDetails;
+import org.teleal.cling.model.meta.*;
 import org.teleal.cling.model.types.DeviceType;
 import org.teleal.cling.model.types.UDADeviceType;
 import org.teleal.cling.model.types.UDN;
 
 import java.io.IOException;
 
-public class CrowdMusicServer {
+public class CrowdMusicClient {
     private LocalDevice localDevice;
 
     public LocalDevice getLocalDevice() {
         return localDevice;
     }
 
-    public CrowdMusicServer() {
+    public CrowdMusicClient() {
         try {
             this.localDevice = createDevice();
         } catch (ValidationException e) {
@@ -36,15 +31,15 @@ public class CrowdMusicServer {
     LocalDevice createDevice() throws ValidationException, LocalServiceBindingException, IOException {
         DeviceIdentity identity =
                 new DeviceIdentity(
-                        UDN.uniqueSystemIdentifier("CrowdMusicServer")
+                        UDN.uniqueSystemIdentifier("CrowdMusicClient")
                 );
 
         DeviceType type =
-                new UDADeviceType("CMSDevice", 1);
+                new UDADeviceType("CMCDevice", 1);
 
         DeviceDetails details =
                 new DeviceDetails(
-                        "CrowdMusicServer",
+                        "CrowdMusicClient",
                         new ManufacturerDetails("HdM"),
                         new ModelDetails(
                                 "CrowdMusic",
@@ -53,7 +48,7 @@ public class CrowdMusicServer {
                         )
                 );
 
-        LocalService<CrowdMusicServerService> crowdMusicService = new AnnotationLocalServiceBinder().read(CrowdMusicServerService.class);
+        LocalService<CrowdMusicServerService> crowdMusicService = new AnnotationLocalServiceBinder().read(CrowdMusicClientService.class);
         crowdMusicService.setManager(new DefaultServiceManager(crowdMusicService, CrowdMusicServerService.class));
 
         return new LocalDevice(identity, type, details, crowdMusicService);
