50db01f96cb37356d483a5f13968ac57e498f191
Sascha Can <Sascha.Can@googlemail.com>
2013-12-11 03:15:50 +0100

    get post data

--- CrowdMusic/src/main/java/com/hdm/crowdmusic/core/streaming/PostAudioHandler.java
+++ CrowdMusic/src/main/java/com/hdm/crowdmusic/core/streaming/PostAudioHandler.java
@@ -3,6 +3,7 @@ package com.hdm.crowdmusic.core.streaming;
 import android.content.Context;
 import android.util.Log;
 
+import com.hdm.crowdmusic.core.CrowdMusicTrack;
 import com.hdm.crowdmusic.util.Utility;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
@@ -13,7 +14,6 @@ import org.apache.http.HttpStatus;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpRequestHandler;
 import org.apache.http.util.EntityUtils;
-
 import java.io.IOException;
 import java.util.Locale;
 
@@ -36,10 +36,32 @@ public class PostAudioHandler implements HttpRequestHandler {
 
         if (httpRequest instanceof HttpEntityEnclosingRequest) {
             HttpEntity entity = ((HttpEntityEnclosingRequest) httpRequest).getEntity();
-            String body = EntityUtils.toString(entity);
-            Log.i(Utility.LOG_TAG_HTTP, body);
+            CrowdMusicTrack track = getPostData(entity);
+            Log.i(Utility.LOG_TAG_HTTP, "ID: " + track.getId() + " | IP: " + track.getIp() + " | Artist: " + track.getArtist() + " | Track: " + track.getTrackName());
         }
 
         httpResponse.setStatusCode(HttpStatus.SC_OK);
+    }
+
+    private CrowdMusicTrack getPostData(HttpEntity entity) {
+        int id;
+        String ip;
+        String artist;
+        String trackName;
+
+        try {
+            String postData = EntityUtils.toString(entity);
+            String[] parameters = postData.split("&");
+
+            id = Integer.parseInt(parameters[0].replace("id=", ""));
+            ip = parameters[1].replace("ip=", "");
+            artist = parameters[2].replace("artist=", "").replace("+", " ").replace("%3C", "<").replace("%3E", ">");
+            trackName = parameters[3].replace("track=", "").replace("+", " ");
+
+            return new CrowdMusicTrack(id, ip, artist, trackName);
+        } catch (IOException e) {
+            Log.e(Utility.LOG_TAG_HTTP, e.getMessage());
+            return null;
+        }
     }
 }
